7c0cfa858fac388fa36bc7627c5c98b559fe4b58
FanYucai <fanyucai@126.com>
2016-10-17 23:43:07 +0800

    modified several funcs, now dblmul is okay

--- fanyc.py
+++ fanyc.py
@@ -7,43 +7,66 @@ def addGaussian(num, s, sigma): # mu=0
     s_res = s + offset_Guassian
     return s_res
 
-def genDataInRand(num=10, xrange=2, yrange=1, sigma=0.05):
+def genDataInRand(num=20, xrange=3, yrange=1, sigma=0.05):
     interv = xrange*1.0/num
     t = np.arange(0, xrange, interv) #here t is not uniform
     s = yrange*np.sin(2*np.pi*t)
     s = addGaussian(s.shape[0], s, sigma)
     return t, s
 
-def genData(num=10, xrange=2, yrange=1, sigma=0.05):
+def genData(num=20, xrange=3, yrange=1, sigma=0.05):
     interv = xrange*1.0/num
     t = np.arange(0, xrange, interv)
     s = yrange*np.sin(2*np.pi*t)
     s = addGaussian(s.shape[0], s, sigma)
     return t, s
 
-def calWithWeigh(x, weigh):
-    order = weigh.shape[0]
+def calWithWeight(x, weight, max_power):
+    y = []
+    for iter_x in x:
+        sum = 0.0
+        for i in range(0, max_power+1):
+            sum += 1.0*np.power(iter_x, i)*weight[i]
+        print 'sum:'
+        print sum
+        y.append(sum)
     return y
 
 def minMul(t, s, max_power):
     A = []
-    for i in range(0, max_power+1):
+    size = t.shape[0]
+    print size
+
+    for k in range(0, max_power+1):
         A_sub = []
         for j in range(0, max_power+1):
-            A_sub.append(np.power(t[i], j))
+            sum_t = 0
+            for i in range(0, size):
+                sum_t += np.power(t[i], j+k)
+            A_sub.append(sum_t)
         A.append(A_sub)
-    mat_A = np.mat(A)
-    # print A
+    A = np.array(A)
 
-    mat_B = np.transpose(np.mat(s))
-    # print B
 
-    weigh_res = np.linalg.solve(mat_A, mat_B)
+    B = []
+    for k in range(0, max_power+1):
+        sum_s = 0
+        for i in range(0, size):
+            sum_s += np.power(t[i], k)*s[i]
+        B.append(sum_s)
+    B = np.array(B)
 
-    # x = np.arange(0, xrange, 0.01)
-    x = np.arange(0, 2, 0.01)
-    y = calWithWeigh(x, weigh_res)
-    return x
+    print A.shape, B.shape
+    weight_res = np.linalg.solve(A, B)
+    #>>>>>>>>>>>>>>>>>>>
+    x = np.arange(0, 3, 0.01)
+    #>>>>>>>>>>>>>>>>>>>
+
+    # print weigh_res.shape
+    # print weigh_res
+
+    y = calWithWeight(x, weight_res, max_power)
+    return x, y
 #
 # def minMulPenalty(t, s, max_power):
 #     return
@@ -60,18 +83,19 @@ def minMul(t, s, max_power):
 if __name__=="__main__":
     # data_num, data_xr, data_yr, data_thetaoff, data_sigma, data_yoff
 
-    t, s = genData(20)
+    t, s = genData(50)
     plt.plot(t, s, 'go')
 
-    base_t = np.arange(0, 2, 0.001)
+    base_t = np.arange(0, 3, 0.001)
     base_s = np.sin(2 * np.pi * base_t)
 
-    plt.axis([0, 2, -1.5, 1.5])
+    plt.axis([0, 3, -1.5, 1.5])
     plt.plot(base_t, base_s, 'r-')
     # plt.plot(np.arange(0, data_xr, 0.01), 5*np.sin(2*np.pi*t+data_theta)+data_yoff, 'g-')
 
-    minMul(t, s, 6)
+    minMul_t, minMul_s = minMul(t, s, 20)
 
+    plt.plot(minMul_t, minMul_s, 'b-')
 
     plt.show()
 
