8488a495d5a05e0fbd4ef97dbe47d32d32329cca
FanYucai <fanyucai@126.com>
2016-10-17 20:31:43 +0800

    nothing, frames

---
+++ fanyc.py
@@ -0,0 +1,30 @@
+import numpy as np
+import matplotlib as plt
+
+def addGaussian(s)
+    for iter_s in s:
+        
+    return s_res
+
+def genSin(num=50, xrange=20, yrange=5, theta_offset=0):
+    t = np.arange(0, xrange/num, xrange)
+    s = yrange*np.sin(2*np.pi*t + theta_offset)
+
+    return t, s
+
+def minMul(t, s, max_power):
+    return
+
+def minMulPenalty(t, s, max_power):
+    return
+
+def gradiantDescent(t, s, max_power, alpha, iter_times):
+    return
+
+def conjGradiant(t, s, max_power, alpha, iter_times):
+    return
+
+def otherFunc()
+    return
+
+if __name__=="__main__":
